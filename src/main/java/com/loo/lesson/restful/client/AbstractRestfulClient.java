c5961dffe3f448283eed965aa8e41c050e2c8f3d
bluex <hallowin9527@gmail.com>
2018-08-06 20:33:33 +0800

    新增post

--- src/main/java/com/loo/lesson/restful/client/AbstractRestfulClient.java
+++ src/main/java/com/loo/lesson/restful/client/AbstractRestfulClient.java
@@ -1,18 +1,22 @@
 package com.loo.lesson.restful.client;
 
 import com.loo.lesson.restful.adapter.rpc.service.RestfulHandlerAdaptRpcService;
+import com.loo.lesson.restful.client.utils.RestfulClientUtils;
 import com.loo.lesson.restful.filter.RestfulFilterChain;
 import com.loo.lesson.restful.vo.request.RestfulRequest;
+import com.loo.lesson.restful.vo.response.RestfulRespMessage;
 import net.sf.json.JSONObject;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.InitializingBean;
 import tomax.loo.lesson.cache.service.centralize.CentralizeCacheService;
 
 import javax.annotation.Resource;
+import java.util.Map;
 
 /**
  * @program: base-core
@@ -34,22 +38,83 @@ public abstract class AbstractRestfulClient implements InitializingBean, BeanFac
     @Resource(name = "restfulFilterChain")
     private RestfulFilterChain chain;
 
+    /**
+     * 使用post 方法获取指定对象
+     * @param requestParam
+     * @param responseType
+     * @param <T>
+     * @return
+     * @throws RestfulException
+     */
     @SuppressWarnings("unchecked")
-    protected <T> T postForObject(RestfulRequest requestParam, Class<T> responstType) throws RestfulException {
+    protected <T> T postForObject(RestfulRequest requestParam, Class<T> responseType) throws RestfulException {
         try {
             String response = postForJSONObject(requestParam);
             if (StringUtils.isBlank(response)) {
                 return null;
             }
             JSONObject obj = JSONObject.fromObject(response);
-            return (T) JSONObject.toBean(obj, responstType);
+            return (T) JSONObject.toBean(obj, responseType);
         } catch (Exception e) {
             logger.error("使用post 方法获取指定对象");
             throw new RestfulException("使用post 方法获取指定对象", e);
         }
     }
 
-    private String postForJSONObject(RestfulRequest requestParam) {
+    /**
+     * 使用post 方法获取指定对象
+     * @param requestParam
+     * @param responseType
+     * @param classMap
+     * @param <T>
+     * @return
+     * @throws RestfulException
+     */
+    @SuppressWarnings("unchecked")
+    protected <T> T postForObject(RestfulRequest requestParam, Class<T> responseType, Map classMap) throws RestfulException {
+        if (classMap != null) {
+            return this.postForObject(requestParam, responseType);
+        }
+        try {
+            String response = postForJSONObject(requestParam);
+            if (StringUtils.isBlank(response)) {
+                return null;
+            }
+            JSONObject obj = JSONObject.fromObject(response);
+            return (T) JSONObject.toBean(obj, responseType, classMap);
+        } catch (Exception e) {
+            logger.error("使用post 方法获取指定对象");
+            throw new RestfulException("使用post 方法获取指定对象", e);
+        }
+    }
+
+    protected RestfulRespMessage postForRespMessage(RestfulRequest requestParam) throws RestfulException {
+        try {
+            String response = postForJSONObject(requestParam);
+            if (StringUtils.isBlank(response)) {
+                return null;
+            }
+            return RestfulClientUtils.getResultRespMessage(response);
+        } catch (Exception e) {
+            logger.error("使用post 方法获取RestfulRespMessage对象 异常", e);
+            throw new RestfulException("使用post 方法获取RestfulRespMessage对象 异常");
+        }
+    }
+
+    public String postForJSONObject(RestfulRequest requestParam) throws RestfulException {
+        if (this.restfulHandlerAdaptRpcService != null) {
+
+        }
         return null;
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
     }
 }
--- src/main/java/com/loo/lesson/restful/client/utils/RestfulClientUtils.java
+++ src/main/java/com/loo/lesson/restful/client/utils/RestfulClientUtils.java
@@ -7,10 +7,13 @@ import com.loo.lesson.restful.vo.response.RestfulRespMessage;
 import org.apache.commons.lang.StringUtils;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.JavaType;
+import org.codehaus.jackson.type.TypeReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * @program: base-core
@@ -64,6 +67,25 @@ public class RestfulClientUtils {
         return null;
     }
 
+    /**
+     * 获取json RestfulRespMessage格式对象
+     * @param jsonStr
+     * @return
+     */
+    public static RestfulRespMessage getResultRespMessage(String jsonStr) {
+        if (StringUtils.isBlank(jsonStr)) {
+            logger.error("获取json RestfulRespMessage格式对象字符串为空， jsonStr={}", jsonStr);
+        }
+        return JacksonUtils.jsonObjUnSerializer(jsonStr, RestfulRespMessage.class);
+    }
+
+    /**
+     * 获取json RestfulRespMessage格式(body DataType)对象
+     * @param jsonStr
+     * @param bodyDataType
+     * @param <T>
+     * @return
+     */
     public static <T> RestfulRespMessage getResultRespMessage(String jsonStr, Class<T> bodyDataType) {
         if (StringUtils.isBlank(jsonStr)) {
             logger.error("获取json RestfulRespMessage格式(body DataType)对象字符串为空");
@@ -85,11 +107,80 @@ public class RestfulClientUtils {
             return message;
         } catch (Exception e) {
             logger.error("获取json RestfulRespMessage格式(body DataType)，jsonStr={}", jsonStr, e);
-
         }
         return null;
     }
 
+    /**
+     * 获取json RestfulRespMessage格式(body DataType)对象
+     * @param jsonStr
+     * @param bodyDataType
+     * @param <T>
+     * @return
+     */
+    public static <T> RestfulRespMessage getResultRespMessage(String jsonStr, JavaType bodyDataType) {
+        if (StringUtils.isBlank(jsonStr)) {
+            logger.error("获取json RestfulRespMessage格式(body DataType)对象字符串为空");
+            return null;
+        }
+        try {
+            ObjectMapper mapper = JacksonUtils.getObjectMapper();
+            JsonNode messageNode = mapper.readTree(jsonStr);
+            RestfulRespMessage message = parseResultRespMessage(messageNode);
+            if (message.getBody() == null) {
+                return message;
+            }
+            JsonNode bodyNode = messageNode.path("body");
+            JsonNode dataNode = bodyNode.path("data");
+            if (dataNode == null) {
+                return message;
+            }
+            message.getBody().setData(JacksonUtils.jsonObjUnSerializer(dataNode, bodyDataType));
+            return message;
+        } catch (Exception e) {
+            logger.error("获取json RestfulRespMessage格式(body DataType)，jsonStr={}", jsonStr, e);
+        }
+        return null;
+    }
+
+    /**
+     * 获取json RestfulRespMessage格式
+     * eg:
+     * new TypeReference<List<A>>() {}
+     * @param jsonStr
+     * @param typeReference
+     * @param <T>
+     * @return
+     */
+    public static <T> RestfulRespMessage getResultRespMessage(String jsonStr, TypeReference<T> typeReference) {
+        if (StringUtils.isBlank(jsonStr)) {
+            logger.error("获取json RestfulRespMessage格式(body DataType)对象字符串为空");
+            return null;
+        }
+        try {
+            ObjectMapper mapper = JacksonUtils.getObjectMapper();
+            JsonNode messageNode = mapper.readTree(jsonStr);
+            RestfulRespMessage message = parseResultRespMessage(messageNode);
+            if (message.getBody() == null) {
+                return message;
+            }
+            JsonNode bodyNode = messageNode.path("body");
+            JsonNode dataNode = bodyNode.path("data");
+            if (dataNode == null) {
+                return message;
+            }
+            message.getBody().setData(JacksonUtils.jsonObjUnSerializer(dataNode, typeReference));
+            return message;
+        } catch (Exception e) {
+            logger.error("获取json RestfulRespMessage格式(body DataType)，jsonStr={}", jsonStr, e);
+        }
+        return null;
+    }
+    /**
+     * 解析通用响应消息数据
+     * @param messageNode
+     * @return
+     */
     private static RestfulRespMessage parseResultRespMessage(JsonNode messageNode) {
         try {
             JsonNode headNode = messageNode.path("head");
@@ -122,5 +213,20 @@ public class RestfulClientUtils {
             logger.error("获取json RestfulRespMessage格式(body DataType)", e);
         }
         return null;
+    }
+
+    /**
+     * 获取json RestfulRespMessage格式(body data为list对象，list元素为dataListType类型)对象.
+     * @param jsonStr
+     * @param dataListType
+     * @param <T>
+     * @return
+     */
+    public static <T> RestfulRespMessage getRestfulRespMessageDataList(String jsonStr, Class<T> dataListType) {
+        if (StringUtils.isBlank(jsonStr)) {
+            logger.error("获取json RestfulRespMessage格式(body data为List对象，list元素为dataListType类型)对象字符串为空");
+            return null;
+        }
+        return getResultRespMessage(jsonStr, JacksonUtils.getCollectionType(List.class, dataListType));
     }
 }
